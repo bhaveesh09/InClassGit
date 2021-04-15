b384ac74608257369477a58fc49c7797d2c7ce6a
bhaveesh09 <bhaveesh.09@gmail.com>
2021-04-15 16:52:38 -0700

    sub

--- calc.py
+++ calc.py
@@ -1,3 +1,17 @@
 def calc(a, b):
     sum = (a + b)
     print(sum)
+
+def sub(a, b):
+    subtract = (a - b)
+    print(subtract)
+
+def div(a, b):
+    divide = (a/b)
+    print(divide)
+
+def mul(a, b):
+    multiply = (a*b)
+    print(multiply)
+
+
